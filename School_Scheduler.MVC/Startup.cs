c922064a009f3c212c94429ab5d62b4b45d6cd75
Everett Grassler <dragwar9@gmail.com>
2019-06-16 18:50:55 -0500

    Add project files.

---
+++ School_Scheduler.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(School_Scheduler.MVC.Startup))]
+namespace School_Scheduler.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
