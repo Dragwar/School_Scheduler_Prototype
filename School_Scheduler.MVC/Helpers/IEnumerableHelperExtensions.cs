58213aa7ba0e943626483fa80cbc0b59b932e3e2
Everett Grassler <dragwar9@gmail.com>
2019-06-18 15:27:58 -0500

    Added pick random helper

---
+++ School_Scheduler.MVC/Helpers/IEnumerableHelperExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace School_Scheduler.MVC.Helpers
+{
+    public static class IEnumerableHelperExtensions
+    {
+        public static T PickRandom<T>(this IEnumerable<T> items) where T : class => items?.OrderBy(item => Guid.NewGuid()).FirstOrDefault();
+        public static T PickRandom<T>(this IEnumerable<T> items, Random rng) where T : class => items?.OrderBy(item => rng.Next()).FirstOrDefault();
+    }
+}
