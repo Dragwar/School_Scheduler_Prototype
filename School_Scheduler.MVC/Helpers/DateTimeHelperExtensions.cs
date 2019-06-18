a41d36d1b4d9706f728d373e5b43528f8a0c9988
Everett Grassler <dragwar9@gmail.com>
2019-06-18 01:47:49 -0500

    Made a DateTimeExtension class and a InRange Method

---
+++ School_Scheduler.MVC/Helpers/DateTimeHelperExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using School_Scheduler.MVC.Models.Domain;
+using School_Scheduler.MVC.Models.ViewModels;
+
+namespace School_Scheduler.MVC.Helpers
+{
+    public static class DateTimeHelperExtensions
+    {
+        public static bool InRange(this DateTime dateToCheck, DateTime startDate, DateTime endDate) => dateToCheck >= startDate && dateToCheck < endDate;
+        public static bool InRange(this DateTime dateToCheck, Course course) => dateToCheck >= course.StartDate && dateToCheck < course.EndDate;
+        public static bool InRange(this DateTime dateToCheck, CourseViewModel course) => dateToCheck >= course.StartDate && dateToCheck < course.EndDate;
+        public static bool InRange(this DateTime dateToCheck, SchoolProgram program)
+        {
+            DateTime firstDateTime = program.Courses
+                .Select(course => course.StartDate)
+                .OrderBy(dt => dt)
+                .First();
+
+            DateTime lastDateTime = program.Courses
+                .Select(course => course.StartDate)
+                .OrderBy(dt => dt)
+                .Last();
+
+            return dateToCheck.InRange(firstDateTime, lastDateTime);
+        }
+    }
+}
