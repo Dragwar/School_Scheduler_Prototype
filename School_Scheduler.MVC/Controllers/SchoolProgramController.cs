dcbcfa13cebea0235b0556bd5587cd0a0cce34c3
Jashan Saraon <Jashanp.k99@gmail.com>
2019-06-20 14:05:56 -0500

    Added SchoolProgram Actions and Create/Edit ViewModel (No views made)

--- School_Scheduler.MVC/Controllers/SchoolProgramController.cs
+++ School_Scheduler.MVC/Controllers/SchoolProgramController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using School_Scheduler.MVC.Models;
+using School_Scheduler.MVC.Models.Domain;
+using School_Scheduler.MVC.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +10,141 @@ using System.Web.Mvc;
 
 namespace School_Scheduler.MVC.Controllers
 {
+
+    // TODO: You must check the user's discriminator (Instructor)
     public class SchoolProgramController : Controller
     {
-        // GET: SchoolProgram
+        private ApplicationDbContext DbContext;
+        public SchoolProgramController()
+        {
+            DbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateProgram()
+        {
+            // TODO: You must check the user's discriminator (Instructor)
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateProgram(CreateEditSchoolProgramViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            model.Name = model.Name.Trim();
+            if (DbContext.SchoolPrograms.Any(p =>
+            p.Name.ToLower() == model.Name.ToLower()))
+            {
+                ModelState.AddModelError(nameof(CreateEditSchoolProgramViewModel.Name),
+                    "Program Name should be unique");
+                return View(model);
+            }
+
+            SchoolProgram program;
+            program = new SchoolProgram();
+            program.Name = model.Name;
+
+            DbContext.SchoolPrograms.Add(program);
+            DbContext.SaveChanges();
+            return View();
+        }
+
+        //[HttpGet]
+        //public ActionResult EditProgram(string name)
+        //{
+        //    var program = DbContext.SchoolPrograms.FirstOrDefault(
+        //        p => p.Name == name);
+        //    if (program == null)
+        //    {
+        //        return RedirectToAction(nameof(SchoolProgramController.Index));
+        //    }
+        //    var model = new CreateEditSchoolProgramViewModel();
+        //    model.Name = program.Name;
+        //    return View(model);
+        //}
+
+        [HttpGet]
+        public ActionResult EditProgram(Guid id)
+        {
+            var program = DbContext.SchoolPrograms.FirstOrDefault(
+                p => p.Id == id);
+            if (program == null)
+            {
+                ModelState.AddModelError("", "School Program not found...");
+                return View("Error");
+            }
+            string userId = User.Identity.GetUserId();
+            if (!program.Instructors.Any(p => p.Id == userId))
+            {
+                ModelState.AddModelError("", "You can't edit a School Program that you don't belong to");
+                return View("Error");
+            }
+
+            var model = new CreateEditSchoolProgramViewModel
+            {
+                Name = program.Name
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditProgram(Guid id, CreateEditSchoolProgramViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            model.Name = model.Name.Trim();
+            if (DbContext.SchoolPrograms.Any(p =>
+            p.Name.ToLower() == model.Name.ToLower()))
+            {
+                ModelState.AddModelError(nameof(CreateEditSchoolProgramViewModel.Name),
+                    "Program Name should be unique");
+                return View(model);
+            }
+
+            SchoolProgram program;
+            program = new SchoolProgram();
+            program.Name = model.Name;
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string name)
+        {
+            var program = DbContext.SchoolPrograms.FirstOrDefault(
+                p => p.Name == name);
+
+            if (program == null)
+            {
+                ModelState.AddModelError("", "School Program not found...");
+                return View("Error");
+            }
+            string userId = User.Identity.GetUserId();
+            if (!program.Instructors.Any(p => p.Id == userId))
+            {
+                ModelState.AddModelError("", "You can't edit a School Program that you don't belong to");
+                return View("Error");
+            }
+
+            DbContext.SchoolPrograms.Remove(program);
+            DbContext.SaveChanges();
+
+            return RedirectToAction(nameof(SchoolProgramController.Index));
         }
     }
 }
---
+++ School_Scheduler.MVC/Models/ViewModels/CreateEditSchoolProgramViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_Scheduler.MVC.Models.ViewModels
+{
+    public class CreateEditSchoolProgramViewModel
+    {
+        public string Name { get; set; }
+    }
+}
