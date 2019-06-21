c23a2ce1a8abde954151fe1e40d1d9e753b1e1ef
Everett Grassler <dragwar9@gmail.com>
2019-06-21 00:36:11 -0500

    Implemented School Program Actions and Views

--- School_Scheduler.MVC/Controllers/SchoolProgramController.cs
+++ School_Scheduler.MVC/Controllers/SchoolProgramController.cs
@@ -1,66 +1,90 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity;
 using School_Scheduler.MVC.Filters;
 using School_Scheduler.MVC.Helpers;
 using School_Scheduler.MVC.Models;
 using School_Scheduler.MVC.Models.Domain;
 using School_Scheduler.MVC.Models.ViewModels;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 
 namespace School_Scheduler.MVC.Controllers
 {
-
-    // TODO: You must check the user's discriminator (Instructor)
     public class SchoolProgramController : Controller
     {
-        private ApplicationDbContext DbContext;
-        public SchoolProgramController()
-        {
-            DbContext = new ApplicationDbContext();
-        }
+        private ApplicationDbContext DbContext { get; } = new ApplicationDbContext();
 
+        [HttpGet]
         [EnsureDiscriminatorClaim(Discriminator.Instructor, Discriminator.Student)]
         public ActionResult Index()
         {
+            string userId = User.Identity.GetUserId();
+            Discriminator userDiscriminator = User.Identity.GetUserDiscriminator();
+            ApplicationUser foundUser = DbContext.Users.FirstOrDefault(user => userId == user.Id);
+
+            // this check isn't really needed because [EnsureDiscriminatorClaim]
+            if (foundUser == null)
+            {
+                ModelState.AddModelError("", "You must be logged in. (If you already are contact an admin for assistance)");
+                return View("Error");
+            }
+
+            List<SchoolProgram> usersSchoolPrograms;
+            switch (foundUser)
+            {
+                case Instructor _: usersSchoolPrograms = DbContext.SchoolPrograms.Where(sp => sp.Instructors.Any(i => i.Id == userId)).ToList(); break;
+                case Student _: usersSchoolPrograms = DbContext.SchoolPrograms.Where(sp => sp.EnrolledStudents.Any(i => i.Id == userId)).ToList(); break;
+
+                // these checks aren't really needed because [EnsureDiscriminatorClaim]
+                case ApplicationUser _: ModelState.AddModelError("", "You must be a Student or Instructor for viewing courses"); return View("Error");
+                default: ModelState.AddModelError("", "You must be logged in. (If you already are contact an admin for assistance)"); return View("Error");
+            }
+
+            ViewBag.SchoolPrograms = usersSchoolPrograms;
+
             return View();
         }
 
         [HttpGet]
         [EnsureDiscriminatorClaim(Discriminator.Instructor)]
-        public ActionResult CreateProgram()
-        {
-            // TODO: You must check the user's discriminator (Instructor)
-            return View();
-        }
+        public ActionResult Create() => View();
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         [EnsureDiscriminatorClaim(Discriminator.Instructor)]
-        public ActionResult CreateProgram(CreateEditSchoolProgramViewModel model)
+        public ActionResult Create(CreateEditSchoolProgramViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
+
             model.Name = model.Name.Trim();
-            if (DbContext.SchoolPrograms.Any(p =>
-            p.Name.ToLower() == model.Name.ToLower()))
+
+            if (DbContext.SchoolPrograms.Any(p => p.Name.ToLower() == model.Name.ToLower()))
             {
-                ModelState.AddModelError(nameof(CreateEditSchoolProgramViewModel.Name),
-                    "Program Name should be unique");
+                ModelState.AddModelError(nameof(CreateEditSchoolProgramViewModel.Name), "Program Name should be unique");
                 return View(model);
             }
 
-            SchoolProgram program;
-            program = new SchoolProgram();
-            program.Name = model.Name;
+            string userId = User.Identity.GetUserId();
+            if (DbContext.Users.FirstOrDefault(user => user.Id == userId) is Instructor foundInstructor)
+            {
+                SchoolProgram schoolProgram = new SchoolProgram
+                {
+                    Name = model.Name,
+                };
+                foundInstructor.SchoolProgram = schoolProgram;
 
-            DbContext.SchoolPrograms.Add(program);
-            DbContext.SaveChanges();
-            return View();
+                DbContext.SchoolPrograms.Add(schoolProgram);
+                DbContext.SaveChanges();
+                return RedirectToAction(nameof(Details), new { schoolProgram.Id });
+            }
+
+            ModelState.AddModelError("", "You must be a Instructor to be able to create School Programs");
+            return View("Error");
         }
 
         //[HttpGet]
@@ -78,24 +102,22 @@ namespace School_Scheduler.MVC.Controllers
         //}
 
         [HttpGet]
-        [EnsureDiscriminatorClaim(Discriminator.Instructor)]
-        public ActionResult EditProgram(Guid id)
+        public ActionResult Edit(Guid id)
         {
-            var program = DbContext.SchoolPrograms.FirstOrDefault(
-                p => p.Id == id);
+            SchoolProgram program = DbContext.SchoolPrograms.FirstOrDefault(sp => sp.Id == id);
             if (program == null)
             {
                 ModelState.AddModelError("", "School Program not found...");
                 return View("Error");
             }
             string userId = User.Identity.GetUserId();
-            if (!program.Instructors.Any(p => p.Id == userId))
+            if (!program.Instructors.Any(i => i.Id == userId))
             {
                 ModelState.AddModelError("", "You can't edit a School Program that you don't belong to");
                 return View("Error");
             }
 
-            var model = new CreateEditSchoolProgramViewModel
+            CreateEditSchoolProgramViewModel model = new CreateEditSchoolProgramViewModel
             {
                 Name = program.Name
             };
@@ -104,59 +126,92 @@ namespace School_Scheduler.MVC.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [EnsureDiscriminatorClaim(Discriminator.Instructor)]
-        public ActionResult EditProgram(Guid id, CreateEditSchoolProgramViewModel model)
+        public ActionResult Edit(Guid id, CreateEditSchoolProgramViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
+
             model.Name = model.Name.Trim();
-            if (DbContext.SchoolPrograms.Any(p =>
-            p.Name.ToLower() == model.Name.ToLower()))
+            if (DbContext.SchoolPrograms.Any(sp => sp.Name.ToLower() == model.Name.ToLower()))
             {
-                ModelState.AddModelError(nameof(CreateEditSchoolProgramViewModel.Name),
-                    "Program Name should be unique");
+                ModelState.AddModelError(nameof(CreateEditSchoolProgramViewModel.Name), "Program Name should be unique");
                 return View(model);
             }
 
-            SchoolProgram program;
-            program = new SchoolProgram();
-            program.Name = model.Name;
-            return View();
-        }
-
-        [EnsureDiscriminatorClaim(Discriminator.Instructor, Discriminator.Student)]
-        public ActionResult Details()
-        {
-
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [EnsureDiscriminatorClaim(Discriminator.Instructor)]
-        public ActionResult Delete(string name)
-        {
-            var program = DbContext.SchoolPrograms.FirstOrDefault(
-                p => p.Name == name);
-
-            if (program == null)
-            {
-                ModelState.AddModelError("", "School Program not found...");
-                return View("Error");
-            }
             string userId = User.Identity.GetUserId();
-            if (!program.Instructors.Any(p => p.Id == userId))
+
+            SchoolProgram foundSchoolProgram = DbContext.SchoolPrograms.FirstOrDefault(sp => sp.Id == id);
+            if (foundSchoolProgram == null)
             {
-                ModelState.AddModelError("", "You can't edit a School Program that you don't belong to");
+                ModelState.AddModelError("", "School Program was not found");
+                return View("Error");
+            }
+            if (!foundSchoolProgram.Instructors.Any(i => i.Id == userId))
+            {
+                ModelState.AddModelError("", "You aren't part of this School Program therefore you can't edit it");
                 return View("Error");
             }
 
-            DbContext.SchoolPrograms.Remove(program);
+            foundSchoolProgram.Name = model.Name;
+
             DbContext.SaveChanges();
 
-            return RedirectToAction(nameof(SchoolProgramController.Index));
+            return RedirectToAction(nameof(Details), new { foundSchoolProgram.Id });
         }
+
+        [HttpGet]
+        [EnsureDiscriminatorClaim(Discriminator.Student, Discriminator.Instructor)]
+        public ActionResult Details(Guid id)
+        {
+            string userId = User.Identity.GetUserId();
+
+            SchoolProgram foundSchoolProgram = DbContext.SchoolPrograms
+                .Include(course => course.EnrolledStudents)
+                .Include(course => course.Courses)
+                .Include(course => course.Instructors)
+                .FirstOrDefault(sp => sp.Id == id);
+
+            if (foundSchoolProgram == null)
+            {
+                ModelState.AddModelError("", "School Program was not found");
+                return View("Error");
+            }
+
+            if (foundSchoolProgram.EnrolledStudents.Any(s => s.Id == userId) || foundSchoolProgram.Instructors.Any(i => i.Id == userId))
+            {
+                ViewBag.SchoolProgram = foundSchoolProgram;
+                return View();
+            }
+
+            ModelState.AddModelError("", "You aren't part of this School Program therefore you can't view it");
+            return View("Error");
+        }
+
+
+        //[HttpPost]
+        //public ActionResult Delete(string name)
+        //{
+        //    var program = DbContext.SchoolPrograms.FirstOrDefault(
+        //        p => p.Name == name);
+
+        //    if (program == null)
+        //    {
+        //        ModelState.AddModelError("", "School Program not found...");
+        //        return View("Error");
+        //    }
+        //    string userId = User.Identity.GetUserId();
+        //    if (!program.Instructors.Any(p => p.Id == userId))
+        //    {
+        //        ModelState.AddModelError("", "You can't edit a School Program that you don't belong to");
+        //        return View("Error");
+        //    }
+
+        //    DbContext.SchoolPrograms.Remove(program);
+        //    DbContext.SaveChanges();
+
+        //    return RedirectToAction(nameof(SchoolProgramController.Index));
+        //}
     }
 }
